90c0395692e54830bde97eb768dae87de4061bda
Rick Kleinhans <Rick Kleinhans>
2020-04-10 01:18:32 +0200

    Add summary row

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,11 +4,10 @@ import { useTable } from 'react-table';
 import Layout from '../components/layout';
 import SEO from '../components/seo';
 import {
-  Countries, Country, countryQuery, calculateGrowthData, getPeriodName,
+  Countries, Country, countryQuery, calculateGrowthData, getPeriodName, sumGrowthData,
 } from '../utilities/getData';
 
-const formatCell = (country: Country, period: number): String => {
-  const growthRate = country.growthRates[period];
+const formatCell = (growthRate: number): String => {
   if (Number.isNaN(growthRate)) {
     return 'No deaths';
   } if (!Number.isFinite(growthRate)) {
@@ -17,39 +16,32 @@ const formatCell = (country: Country, period: number): String => {
   return `${growthRate.toString()}%`;
 };
 
-const IndexPage = () => {
-  const { loading, error, data } = useQuery<Countries>(countryQuery);
-  const growthData = useMemo(() => calculateGrowthData(data), [data]);
-  console.log(growthData);
+const SummaryTable = ({ data }: { data: number[][] }) => {
   const columns = React.useMemo(
     () => [
       {
-        Header: 'Country',
-        accessor: 'name',
-      },
-      {
         Header: getPeriodName(30),
-        accessor: (row: Country) => formatCell(row, 5),
+        accessor: (row: number[]) => formatCell(row[5]),
       },
       {
         Header: getPeriodName(25),
-        accessor: (row: Country) => formatCell(row, 4),
+        accessor: (row: number[]) => formatCell(row[4]),
       },
       {
         Header: getPeriodName(20),
-        accessor: (row: Country) => formatCell(row, 3),
+        accessor: (row: number[]) => formatCell(row[3]),
       },
       {
         Header: getPeriodName(15),
-        accessor: (row: Country) => formatCell(row, 2),
+        accessor: (row: number[]) => formatCell(row[2]),
       },
       {
         Header: getPeriodName(10),
-        accessor: (row: Country) => formatCell(row, 1),
+        accessor: (row: number[]) => formatCell(row[1]),
       },
       {
         Header: getPeriodName(5),
-        accessor: (row: Country) => formatCell(row, 0),
+        accessor: (row: number[]) => formatCell(row[0]),
       },
     ],
     [],
@@ -60,7 +52,103 @@ const IndexPage = () => {
     headerGroups,
     rows,
     prepareRow,
-  } = useTable({ columns, data: growthData });
+  } = useTable({ columns, data });
+  return (
+    <table {...getTableProps()}>
+      <thead>
+        {headerGroups.map((headerGroup) => (
+          <tr {...headerGroup.getHeaderGroupProps()}>
+            {headerGroup.headers.map((column) => (
+              <th {...column.getHeaderProps()}>{column.render('Header')}</th>
+            ))}
+          </tr>
+        ))}
+      </thead>
+      <tbody {...getTableBodyProps()}>
+        {rows.map((row) => {
+          prepareRow(row);
+          return (
+            <tr {...row.getRowProps()}>
+              {row.cells.map((cell) => <td {...cell.getCellProps()}>{cell.render('Cell')}</td>)}
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+};
+
+const FullTable = ({ data }: { data: Country[] }) => {
+  const columns = React.useMemo(
+    () => [
+      {
+        Header: 'Country',
+        accessor: 'name',
+      },
+      {
+        Header: getPeriodName(30),
+        accessor: (row: Country) => formatCell(row.growthRates[5]),
+      },
+      {
+        Header: getPeriodName(25),
+        accessor: (row: Country) => formatCell(row.growthRates[4]),
+      },
+      {
+        Header: getPeriodName(20),
+        accessor: (row: Country) => formatCell(row.growthRates[3]),
+      },
+      {
+        Header: getPeriodName(15),
+        accessor: (row: Country) => formatCell(row.growthRates[2]),
+      },
+      {
+        Header: getPeriodName(10),
+        accessor: (row: Country) => formatCell(row.growthRates[1]),
+      },
+      {
+        Header: getPeriodName(5),
+        accessor: (row: Country) => formatCell(row.growthRates[0]),
+      },
+    ],
+    [],
+  );
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow,
+  } = useTable({ columns, data });
+  return (
+    <table {...getTableProps()}>
+      <thead>
+        {headerGroups.map((headerGroup) => (
+          <tr {...headerGroup.getHeaderGroupProps()}>
+            {headerGroup.headers.map((column) => (
+              <th {...column.getHeaderProps()}>{column.render('Header')}</th>
+            ))}
+          </tr>
+        ))}
+      </thead>
+      <tbody {...getTableBodyProps()}>
+        {rows.map((row) => {
+          prepareRow(row);
+          return (
+            <tr {...row.getRowProps()}>
+              {row.cells.map((cell) => <td {...cell.getCellProps()}>{cell.render('Cell')}</td>)}
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+};
+
+const IndexPage = () => {
+  const { loading, error, data } = useQuery<Countries>(countryQuery);
+  const growthData = useMemo(() => calculateGrowthData(data), [data]);
+  const globalGrowthData = sumGrowthData(growthData);
+
   if (loading) {
     return (
       <Layout>
@@ -80,28 +168,11 @@ const IndexPage = () => {
   return (
     <Layout>
       <SEO title="Latest" />
-      <p>Global rates of change in Covid19 death rate over the six most recent 5-day periods. </p>
-      <table {...getTableProps()}>
-        <thead>
-          {headerGroups.map((headerGroup) => (
-            <tr {...headerGroup.getHeaderGroupProps()}>
-              {headerGroup.headers.map((column) => (
-                <th {...column.getHeaderProps()}>{column.render('Header')}</th>
-              ))}
-            </tr>
-          ))}
-        </thead>
-        <tbody {...getTableBodyProps()}>
-          {rows.map((row) => {
-            prepareRow(row);
-            return (
-              <tr {...row.getRowProps()}>
-                {row.cells.map((cell) => <td {...cell.getCellProps()}>{cell.render('Cell')}</td>)}
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
+      <p>Global rates of change in the Covid19 death count over the six most recent 5-day periods.</p>
+      <p>In total:</p>
+      <SummaryTable data={[globalGrowthData]} />
+      <p>By country:</p>
+      <FullTable data={growthData} />
     </Layout>
   );
 };

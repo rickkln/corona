7282d4b3e755bedc4666a1ef17e4b8260d20da92
Rick Kleinhans <Rick Kleinhans>
2020-04-09 19:32:46 +0200

    Calculate growth data via API

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,33 +1,13 @@
 import React from 'react';
-import { gql, useQuery } from '@apollo/client';
+import { useQuery } from '@apollo/client';
 import Layout from '../components/layout';
 import SEO from '../components/seo';
-
-const today = new Date();
-const periodOne = new Date(new Date().setDate(today.getDate() - 30));
-const apiDateFormat = new Intl.DateTimeFormat('en', { year: 'numeric', month: '2-digit', day: '2-digit' });
-const queryDate = apiDateFormat.format(periodOne);
-
-export const getResults = gql`
-  query ($queryDate: String) {
-    results(date: { gt: $queryDate }) {
-      country {
-        name
-      }
-      date
-      deaths
-    }
-  }
-`;
+import { Countries, getCountries, calculateGrowthData } from './getData';
 
 const IndexPage = () => {
-  const { loading, error, data } = useQuery(
-    getResults,
-    {
-      variables: { queryDate },
-    },
-  );
-  console.log(data);
+  const { loading, error, data } = useQuery<Countries>(getCountries);
+  const growthData = calculateGrowthData(data);
+  console.log(growthData);
   if (loading) {
     return (
       <Layout>

87360f07f52a93b87007bf529eca4a4cda459c5d
Rick Kleinhans <hi@rickkln.com>
2020-04-10 23:23:07 +0200

    Add main copy

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -148,12 +148,11 @@ const IndexPage = () => {
   const { loading, error, data } = useQuery<Countries>(countryQuery);
   const growthData = useMemo(() => calculateGrowthData(data), [data]);
   const globalGrowthData = sumGrowthData(growthData);
-
   if (loading) {
     return (
       <Layout>
         <SEO title="Latest" />
-        <p>Loading</p>
+        <p style={{ textAlign: 'center' }}>Loading</p>
       </Layout>
     );
   }
@@ -165,13 +164,51 @@ const IndexPage = () => {
       </Layout>
     );
   }
+  let BattleStatus;
+  if (globalGrowthData[0] > 0) {
+    BattleStatus = (
+      <p>
+        Is humanity winning the battle yet?
+        {' '}
+        <strong>No.</strong>
+        <br />
+        Unfortunately there were
+        {' '}
+        {globalGrowthData[0]}
+        % more deaths in the last 5 days, than in the 5 days before that.
+        This means pandemic related deaths are still accelerating,
+        and we do not yet have the virus under control.
+      </p>
+    );
+  } else {
+    BattleStatus = (
+      <p>
+        Is humanity winning the battle yet?
+        {' '}
+        <strong>Yes.</strong>
+        <br />
+        Fortunately there were
+        {' '}
+        {globalGrowthData[0]}
+        % less deaths in the last 5 days, than in the 5 days before that.
+        This means pandemic related deaths are slowing down,
+        and we are getting the virus under control.
+      </p>
+    );
+  }
   return (
     <Layout>
       <SEO title="Latest" />
-      <p>Global rates of change in the Covid19 death count over the six most recent 5-day periods.</p>
-      <p>In total:</p>
+      { BattleStatus }
+      <p>
+        These are the changes in the amount new Covid19 deaths
+        over the six most recent 5-day periods globally:
+      </p>
       <SummaryTable data={[globalGrowthData]} />
-      <p>By country:</p>
+      <p>
+        These are the changes in the amount new Covid19 deaths
+        over the six most recent 5-day periods by country:
+      </p>
       <FullTable data={growthData} />
     </Layout>
   );
